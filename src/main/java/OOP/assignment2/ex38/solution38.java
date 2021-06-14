37015ccf10ef0fd1370f3c76fcd4edba53fb0bd6
andres <andres.rosales@knights.ucf.edu>
2021-06-13 21:30:50 -0400

    testing ex38

--- src/main/java/OOP/assignment2/ex38/solution38.java
+++ src/main/java/OOP/assignment2/ex38/solution38.java
@@ -7,6 +7,29 @@ import java.util.*;
 
 public class solution38 {
     public static void main(String[] args) {
+        List<Integer> list = getListNums();
+        List filteredList = filterEvenNumbers(list);
+        System.out.println(filteredList.toString());
+    }
 
+    private static List filterEvenNumbers(List<Integer> list) {
+        for(int i = 0; i < list.size();i++){
+            if(!(list.get(i)%2 == 0) ){
+                list.remove(i);
+            }
+        }
+        return list;
+    }
+
+    public static List getListNums() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter a list of numbers separated by a space \n");
+        String list = scanner.nextLine();
+        String[] elements = list.split(" ");
+        LinkedList<Integer> numbers = new LinkedList<Integer>();
+        for(int i=0;i< elements.length;i++){
+            numbers.add(Integer.parseInt(elements[i]));
+        }
+        return  numbers; 
     }
 }
--- src/main/java/OOP/assignment2/ex39/solution39.java
+++ src/main/java/OOP/assignment2/ex39/solution39.java
@@ -1,4 +1,5 @@
 package OOP.assignment2.ex39;
 //List<Map> listOfMap = new ArrayList<>();
+//hashmap made Map<Object,Object> nameMap = new HashMap<>();
 public class solution39 {
 }
